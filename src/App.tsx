e65d189599e2f5f308f6105d410eedeada2b3b18
tony dela cruz <delacruztony.adc@gmail.com>
2021-08-12 23:00:52 +0800

    Applied authenticated route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import LoginPage from "pages/Login";
 import ProfilePage from "pages/ProfilePage";
-
+import AuthenticatedRoute from "components/AuthenticatedRoute";
 import { Routes as RouteMap } from "enums/routes";
 
 import "./App.css";
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path={RouteMap.ROOT} component={LoginPage} />
-        <Route path={RouteMap.PROFILE} component={ProfilePage} />
+        <Route exact path={RouteMap.LOGIN} component={LoginPage} />
+        <AuthenticatedRoute path={RouteMap.PROFILE} component={ProfilePage} />
       </Switch>
     </div>
   );
