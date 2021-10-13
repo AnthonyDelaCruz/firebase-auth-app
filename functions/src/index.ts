271254f62bd020bd2b0dcf2342d4910fb313acbd
tony dela cruz <delacruztony.adc@gmail.com>
2021-10-13 22:38:56 +0800

    [Feature][Tony] - added additional field to auth cloud function trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,22 +5,27 @@ admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", { structuredData: true });
-//   response.send("Hello from Firebase!");
-// });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export const followUser = functions.https.onCall((data, context) => {
+  // follow user from data
+});
 
 export const createUserOnSignUp = functions.auth
   .user()
   .onCreate(async (user, context) => {
     try {
       const userId = user.uid;
-      await admin
-        .firestore()
-        .collection("users")
-        .doc(userId)
-        .set({ profilePicUrl: "", mobileNumber: "" });
+      await admin.firestore().collection("users").add({
+        uid: userId,
+        profilePicUrl: "",
+        followers: [],
+        following: [],
+      });
     } catch (error) {
       functions.logger.error("Error after creatig user", error);
     }
