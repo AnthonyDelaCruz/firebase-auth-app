55465ea62c9875bd31fae1f634e46ac4435c749d
tony dela cruz <delacruztony.adc@gmail.com>
2021-10-16 19:10:54 +0800

    [Feature][Tony] - Updated login code, removed un-used code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,24 @@
 import React from "react";
 
-import { Typography, Divider } from "antd";
-// import { FireFilled } from "@ant-design/icons";
+import { Divider } from "antd";
 
-// import PersistenceList from "components/PersistenceList";
-// import SignUpForm from "components/SignUpForm";
-// import LoginForm from "components/LoginForm";
-// import EmailLinkSignInForm from "components/EmailLinkSignInForm";
 // import AuthPersistenceWrapper from "wrappers/AuthPersistenceWrapper";
-import { SignUpForm, SocialLoginButtons } from "components";
+import {
+  // EmailLinkSignInForm,
+  // LoginForm,
+  // PersistenceList,
+  SignUpForm,
+  SocialLoginButtons,
+} from "components";
 
 import "./Login.css";
-
-const { Title, Text } = Typography;
 
 function LoginPage() {
   return (
     <div id="login-page">
       <img src="firebase_logo.png" alt="firebase-logo" />
       <div className="login-form">
-        <Title level={2}>Firebase Auth</Title>
-        {/* <Title>
-          Firebase Auth
-          <FireFilled />
-        </Title>
-        <Text>
-          This will be a React Application that will feature firebase auth
-          functionality.
-        </Text> */}
+        <Divider>Login with</Divider>
         <SocialLoginButtons />
         <Divider>or Sign up</Divider>
         <SignUpForm />
