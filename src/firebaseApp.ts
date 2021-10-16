f35dcc13c29df1e1cb13c1b09c826cb9133ca810
tony dela cruz <delacruztony.adc@gmail.com>
2021-10-16 22:46:54 +0800

    [Feature][Tony] - added emulators setup for local development

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -3,13 +3,21 @@ import { firebaseConfig } from "firebaseConfig";
 
 import "firebase/auth";
 import "firebase/functions";
+import "firebase/firestore";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-export const firebaseFunctions = firebaseApp.functions();
-// firebaseFunctions.useEmulator("localhost", 5001); // turn this on to test functions locally
 
+export const firebaseFunctions = firebaseApp.functions();
 export const firebaseAuth = firebaseApp.auth();
+export const firebaseFirestore = firebaseApp.firestore();
+
 export const googleAuthProvider: firebase.auth.GoogleAuthProvider_Instance =
   new firebase.auth.GoogleAuthProvider();
 export const twitterAuthProvider: firebase.auth.TwitterAuthProvider_Instance =
   new firebase.auth.TwitterAuthProvider();
+
+// Emulators setup
+if (window.location.hostname === "localhost") {
+  firebaseFirestore.useEmulator("localhost", 8080);
+  firebaseFunctions.useEmulator("localhost", 5001);
+}
